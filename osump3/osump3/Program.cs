128fb1553ed0dbdbbff55461239aec9d1d0c0607
pixelzerg <abhinavbhandari4@gmail.com>
2017-06-24 22:33:44 +0100

    Init

---
+++ osump3/osump3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace osump3
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ osump3/osump3/Utils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osump3
+{
+    public static class Utils
+    {
+        public static bool Debug = true;
+        public static bool AsciiOnly = false;
+
+    }
+}
